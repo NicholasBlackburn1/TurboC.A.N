0ae0ba16ab9c0e78e013baa09f660f7283eb9b99
Nicholas Blackburn <nickblackburn02@gmail.com>
2021-04-27 16:19:39 -0400

    added gass peddel pos

--- grabcan.py
+++ grabcan.py
@@ -2,14 +2,24 @@
 testing canbus ids to see if it works
 """
 import binascii
+from builtins import print
+from datetime import datetime
 import os
 import struct
 import can
 import logging
 from configparser import ConfigParser
 import time 
+import matplotlib.pyplot as plt
+import numpy
 
+    
+hl, = plt.plot([], [])
 
+def update_line(hl, new_data):
+    hl.set_xdata(numpy.append(hl.get_xdata(), new_data))
+    hl.set_ydata(numpy.append(hl.get_ydata(), new_data))
+    plt.draw()
 #check system name, in linux will print 'posix' and in windows will print 'nt'
 print(os.name)
 logging.debug("CanBus Testing starting...")
@@ -20,11 +30,13 @@ can0 = can.interface.Bus(channel = 'can0', bustype = 'socketcan_ctypes')
 for msg in can0:
     
     # This is the vehical data and ids from canbus
-    data = (binascii.hexlify(msg.data), 16)
+    time = datetime.now().second
+    data = (binascii.hexlify(msg.data))
     id = int(msg.arbitration_id)
-
-
-
-    print (str(id) + "data:"+ " "+ str(data))
+    
+    #print(id)
+    #print(can0.recv(1))
+    if( id == 1041):
+        print(str(id) + "data:"+ "     "+ str(data)+ str(time))
     
         
