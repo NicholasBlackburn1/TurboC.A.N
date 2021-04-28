cd5f52cf438e18d49de26e441619bfa5474a6f7c
Nicholas Blackburn <nickblackburn02@gmail.com>
2021-04-28 13:25:11 -0400

    getting Data to Hopefully graph

--- grabcan.py
+++ grabcan.py
@@ -13,67 +13,66 @@ from configparser import ConfigParser
 import time 
 import matplotlib.pyplot as plt
 import numpy
+import threading
 
+from drivetrain.drivetrain import steeringWheelDataFine, steeringWheelDataGeneral
 
 logging.basicConfig(filename="logs/"+datetime.now().strftime(
         "%Y_%m_%d-%I_%M_%S_%p_%s")+".log", level=logging.DEBUG)
-
-hl, = plt.plot([], [])
 
 def update_line(hl, new_data):
     hl.set_xdata(numpy.append(hl.get_xdata(), new_data))
     hl.set_ydata(numpy.append(hl.get_ydata(), new_data))
     plt.draw()
 
-
-# gets data from Steering System and retunds the rounding info
-def steeringWheelDataGeneral(id,data):
-    #Data to Ints UwU
-    return float(binascii.hexlify(data[2:4]))
-
-def steeringWheelDataFine(id,data):
-    #Data to Ints UwU
-    return float(binascii.hexlify(data[:2]))
-
-
-def gassPeddleDataGeneral(id,data):
-     #Data to Ints UwU
-    pos =float(binascii.hexlify(data[6:8]))
-    return pos
-
-def gassPeddleData(id,data):
-     #Data to Ints UwU
-    pos =float(binascii.hexlify(data[6]))
-    return pos
+def scatterData():
+        plt.draw()
+        plt.pause(0)
+        fig.show()
+        return
 
 #check system name, in linux will print 'posix' and in windows will print 'nt'
 print(os.name)
+fig, ax = plt.subplots()
+ax.set(xlabel='Rpms i think', ylabel='Active',title='Car Engine Data')
+
 logging.debug("CanBus Testing starting...")
 
 logging.debug("CanBus Starting can network...")
 #while True:
 can0 = can.interface.Bus(channel = 'can0', bustype = 'socketcan_ctypes')
 for msg in can0:
-    
+   
     # This is the vehical data and ids from canbus
     id = int(msg.arbitration_id)
     data = (binascii.hexlify(msg.data))
     
-    '''
+    # Steering Wheel Id 
     if(id == 2):
+        logging.warn("getting Data from Wheel")
         print(data[:2])
         print(data[2:4])
-        print('SteringWeel FIne data'+ str(steeringWheelDataFine(id,data)))
-        print("Steering Wheel data General:"+" "+str(steeringWheelDataGeneral(id,data)))
-      
+        print('SteringWeel FIne data'+ str(steeringWheelDataFine(data)))
+        print("Steering Wheel data General:"+" "+str(steeringWheelDataGeneral(data)))
+        plt.scatter(steeringWheelDataGeneral(data),steeringWheelDataFine(data))
+        time.sleep(00.5)
+        logging.warn("Got Data But Now Graphing Hopedfully")
+        
+ 
+scatterData()
+             
+'''
     if(id == 1041):
        print(data)
        print("gas peddle Data"+"  "+str(gassPeddleDataGeneral(id,data)))
-    '''
-
+    
     #logging.debug(str(id) +" "+ str(data))
     if(id == 1040):
+        
         print(data)
-        print(data[:1])
-        print(data[:2])
+        print("data 1byte set:"+str(data[1]))
+        print("data 2byte set:"+str(data[2]))
+        #print(data[:2])
         logging.info(str(id) +" "+str(data))
+'''
+
--- src/drivetrain/drivetrain.py
+++ src/drivetrain/drivetrain.py
@@ -6,14 +6,22 @@ Drivetrain Data Grabbing
 import binascii
 
 
-
 # gets data from Steering System and retunds the rounding info
 def steeringWheelDataGeneral(data):
     #Data to Ints UwU
     return float(binascii.hexlify(data[2:4]))
-
-# gets smaller wheel rotations for finer control of wheel
+#gets Finer Data From Stearing Wheel
 def steeringWheelDataFine(data):
     #Data to Ints UwU
     return float(binascii.hexlify(data[:2]))
 
+#gets General Data From Stearing Wheel
+def gassPeddleDataGeneral(data):
+     #Data to Ints UwU
+    pos =float(binascii.hexlify(data[6:8]))
+    return pos
+
+def gassPeddleData(data):
+     #Data to Ints UwU
+    pos =float(binascii.hexlify(data[6]))
+    return pos
