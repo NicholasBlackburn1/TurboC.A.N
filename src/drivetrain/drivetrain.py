e61de559f7f8d7df2e0ecb7d537f8e61f7c48005
Nicholas Blackburn <nickblackburn02@gmail.com>
2021-04-28 08:08:44 -0400

    added gitignore

--- src/drivetrain/drivetrain.py
+++ src/drivetrain/drivetrain.py
@@ -12,3 +12,10 @@ def steeringWheelData(id,data):
     if(id == 2):
         steering = int(binascii.hexlify(data[:1]),16)+ int(binascii.hexlify(data[:2]),16)*255/26 
         return steering
+
+
+def gassPeddleData(id,data):
+     #Data to Ints UwU
+    if(id == 1041):
+        gas = int(binascii.hexlify(data[:3]),16)+ int(binascii.hexlify(data[:4]),16)*255/26 
+        return gas
