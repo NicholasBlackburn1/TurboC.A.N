ec29a7702c6c0e1af292585a2fbbb5e8acd01008
Nicholas Blackburn <nickblackburn02@gmail.com>
2021-04-28 08:40:27 -0400

    got grabbing bytes correctly

--- grabcan.py
+++ grabcan.py
@@ -12,9 +12,25 @@ from configparser import ConfigParser
 import time 
 import matplotlib.pyplot as plt
 import numpy
-import drivetrain.drivetrain
-    
-    
+
+hl, = plt.plot([], [])
+
+def update_line(hl, new_data):
+    hl.set_xdata(numpy.append(hl.get_xdata(), new_data))
+    hl.set_ydata(numpy.append(hl.get_ydata(), new_data))
+    plt.draw()
+
+
+# gets data from Steering System and retunds the rounding info
+def steeringWheelData(id,data):
+    #Data to Ints UwU
+    return (int(binascii.hexlify(data[:2]),16)+ int(binascii.hexlify(data[2:4]),16)*255/26)
+
+
+def gassPeddleData(id,data):
+     #Data to Ints UwU
+    return  int(binascii.hexlify(data[:3]))
+
 #check system name, in linux will print 'posix' and in windows will print 'nt'
 print(os.name)
 logging.debug("CanBus Testing starting...")
@@ -26,9 +42,11 @@ for msg in can0:
     
     # This is the vehical data and ids from canbus
     id = int(msg.arbitration_id)
-    data = msg.data
-
-    print(drivetrain.drivetrain.steeringWheelData(id,data))
+    data = (binascii.hexlify(msg.data))
     
+    if(id == 2):
+       print(data[:4])
+       print(data[2:4])
+       print("Steering Data"+"  "+str(steeringWheelData(id,data)))
     
         
--- src/drivetrain/drivetrain.py
+++ src/drivetrain/drivetrain.py
@@ -9,13 +9,9 @@ import binascii
 # gets data from Steering System and retunds the rounding info
 def steeringWheelData(id,data):
     #Data to Ints UwU
-    if(id == 2):
-        steering = int(binascii.hexlify(data[:1]),16)+ int(binascii.hexlify(data[:2]),16)*255/26 
-        return steering
+    return (int(binascii.hexlify(data[:1]),16)+ int(binascii.hexlify(data[:2]),16)*255/26)
 
 
 def gassPeddleData(id,data):
      #Data to Ints UwU
-    if(id == 1041):
-        gas = int(binascii.hexlify(data[:3]),16)+ int(binascii.hexlify(data[:4]),16)*255/26 
-        return gas
+    return  int(binascii.hexlify(data[:3]),16)
