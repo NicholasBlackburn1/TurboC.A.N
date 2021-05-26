8a7d9d474ef1ee218c451915f3c318bc99a3a43f
Nicholas Blackburn <nickblackburn02@gmail.com>
2021-05-26 13:17:30 -0400

    got data to work in graphing

--- GraphData.py
+++ GraphData.py
@@ -4,6 +4,9 @@ this file is for graphing data thats been collected
 
 
 from asyncore import read
+from cProfile import label
+
+from matplotlib import colors
 import numpy as np
 import matplotlib.pyplot as plt
 import src.utils.datafileUwU as uwu
@@ -13,22 +16,36 @@ import logging as logger
 
 
 def readavrogas():
-    reader = DataFileReader(open(str("/home/nicholas/Desktop/cardev/collectedData/")+str("gas")+".avro", "rb"), DatumReader())
+    reader = DataFileReader(open(str("/home/nicholas/Desktop/cardev/collectedData/gone/")+str("gas")+".avro", "rb"), DatumReader())
+    index = []
+    finedata = []
+    generaldata = []
+
     for gas in reader:
-        logger.warn("UWU break data")
+        logger.warn("UWU gas data")
         logger.info(gas)
-        print(gas)
-        return gas
+        index.append(int(gas['name']))
+        finedata.append(int(gas['finerPos']))
+        generaldata.append(int(gas['generalPos']))
+
     reader.close()
+    return index,finedata,generaldata
+    
 
         
 def readavrobreak():
-    reader = DataFileReader(open(str("/home/nicholas/Desktop/cardev/collectedData/")+str("break")+".avro", "rb"), DatumReader())
+    reader = DataFileReader(open(str("/home/nicholas/Desktop/cardev/collectedData/gone/")+str("break")+".avro", "rb"), DatumReader())
+    index = []
+    pos = []
     for gas in reader:
         logger.warn("UWU break data")
         logger.info(gas)
-        print (gas)
+        index.append(int(gas['name']))
+        pos.append(int(gas['Pos']))
+        
+
     reader.close()
+    return index,pos
 
 def readavroStearing():
     reader = DataFileReader(open(str("/home/nicholas/Desktop/cardev/collectedData/gone/")+str("stearing")+".avro", "rb"), DatumReader())
@@ -47,20 +64,65 @@ def readavroStearing():
     return index,finedata,generaldata
     
 
+def graphStearing():
 
-index, finer,generaldata = readavroStearing()
+    stindex, stfiner,stgeneraldata = readavroStearing()
 
-x = index
-y = generaldata
-fig, ax = plt.subplots()
+  
+    fig, ax = plt.subplots()
 
-# Using set_dashes() to modify dashing of an existing line
-line1, = ax.plot(x, y, label='Using set_dashes()')
+ 
+    plt.plot(stindex, stfiner,color='pink',label="Stearing Finer Data")
+    plt.plot(stindex, stgeneraldata,color='red',label="Stearing General Data")
+    
+    ax.set_ylabel("indexer counter")
+    ax.set_xlabel("Tics Recording time")
+    ax.legend()
+    plt.title("Stearing Wheel Data")
+    plt.show()
 
-# Using plot(..., dashes=...) to set the dashing when creating a line
-line2, = ax.plot(x, y , label='Using the dashes parameter')
 
-plt.ylabel("indexer counter")
-plt.xlabel("Tics Recording time")
-plt.title("Stearing Wheel Data")
-plt.show()
+
+def graphGas():
+
+    stindex, stfiner,stgeneraldata = readavrogas()
+
+  
+    fig, ax = plt.subplots()
+
+ 
+    plt.plot(stindex, stgeneraldata,color='pink',label="Stearing Finer Data")
+    plt.plot(stindex, stfiner,color='red',label="Stearing General Data")
+    
+    ax.set_ylabel("indexer counter")
+    ax.set_xlabel("Tics Recording time")
+    ax.legend()
+    plt.title("Gas Wheel Data")
+    plt.show()
+
+def breakGraph():
+
+
+    index,Pos = ()
+
+    x = index
+    y = Pos
+    fig, ax = plt.subplots()
+
+    # Using set_dashes() to modify dashing of an existing line
+    line1, = ax.plot(x, y, label='Using set_dashes()')
+
+    # Using plot(..., dashes=...) to set the dashing when creating a line
+    line2, = ax.plot(x, y , label='Using the dashes parameter')
+
+    plt.ylabel("indexer counter")
+    plt.xlabel("Tics Recording time")
+    plt.title("Break Wheel Data")
+    plt.show()
+
+
+
+
+
+
+graphGas()
