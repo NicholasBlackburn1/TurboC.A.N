0a752284c587677874c1213598373eefdd2e4b39
Nicholas Blackburn <nickblackburn02@gmail.com>
2021-05-28 10:20:42 -0400

    added comments

--- Graphing/externalread.py
+++ Graphing/externalread.py
@@ -70,6 +70,33 @@ def ReadgasData():
     return index,generalpos,finerpos
 
 
+
+def ReadStearingData():
+
+    print("Reading  file..")
+
+    reader = DataFileReader(
+    open(str("/home/nicholas/Desktop/cardev/Graphing/output/")+str("stearing")+".avro", "rb"), DatumReader())
+    print("Block count:"+str(reader.block_count)+"\n")
+    print("IS eof:"+str(reader.is_EOF())+"\n")
+    print("read header:"+" "+str(reader._read_header())+"\n")
+    print("File length:"+" "+str(reader.file_length)+"\n")
+    index = []
+    generalpos = []
+    finerpos = []
+    logger.warn("reading file")
+    for gas in reader:
+        index.append(int(gas['name']))
+        generalpos.append(int(gas['generalPos']))
+        finerpos.append(int(gas['finerPos']))   
+    reader.close()
+    print("Index Number:"+str(index)+"\n")
+    print("general:"+" "+ str(generalpos)+"\n")
+    print("finerPos:"+" "+ str(finerpos))
+    return index,generalpos,finerpos
+
+
+
 def graphBreak():
 
     stindex, Pos = ReadbreakData()
@@ -109,5 +136,24 @@ def graphGas():
 
 
 
+def graphStearing():
+
+    stindex, generlpos,finerpos = ReadStearingData()
+
+  
+    fig, ax = plt.subplots()
+
+    plt.plot(stindex, finerpos,color='pink',label="Stearing Finer Data")
+    plt.plot(stindex, generlpos,color='red',label="Stearing General Data")
+    
+    
+    ax.set_ylabel("Wheel Pos")
+    ax.set_xlabel("Tics Recording time ")
+
+    ax.legend()
+    plt.title("Steering Peddel Data")
+    plt.show()
+
+
 #graphGas()
-graphBreak()
+graphStearing()
