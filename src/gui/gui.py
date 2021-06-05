66919fa628cef95bb51be1bf1864c36688ec1f6f
Nicholas Blackburn <nickblackburn02@gmail.com>
2021-06-05 15:05:13 -0400

    added gear display

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -5,10 +5,14 @@ from matplotlib.pyplot import text
 
 import pygame
 import pygame_gui
+
+running = True
 #x,y,width,hight rect 
 Parkrect = pygame.Rect(0,0,100,100)
-Reverserect = pygame.Rect(0,10,100,100)
-Neturalrect = pygame.Rect(0,0,100,100)
+Reverserect = pygame.Rect(130,0,100,100)
+Neturalrect = pygame.Rect(260,0,100,100)
+driverect = pygame.Rect(260,0,100,100)
+
 
 label = pygame.Rect(20,100,100,20)
 pygame.init()
@@ -21,30 +25,33 @@ background.fill(pygame.Color('#000000'))
 
 manager = pygame_gui.UIManager((800, 600))
 
-pygame.draw.rect(background,color='red',rect=parkrect)
+pygame.draw.rect(background,color='red',rect=Parkrect)
+pygame.draw.rect(background,color='red',rect=Reverserect)
+pygame.draw.rect(background,color='red',rect=Neturalrect)
 
 pygame_gui.elements.UILabel(relative_rect=pygame.Rect((0,100,100,50)),text="In Park",manager=manager)
-hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 275), (100, 50)),
-                                            text='Say Hello',
-                                            manager=manager)
+pygame_gui.elements.UILabel(relative_rect=pygame.Rect((130,100,100,50)),text="In Reverse ",manager=manager)
+pygame_gui.elements.UILabel(relative_rect=pygame.Rect((260,100,100,50)),text="In Neutral ",manager=manager)
+
 
 
 clock = pygame.time.Clock()
 is_running = True
 
+
 while is_running:
     time_delta = clock.tick(60)/1000.0
+  
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
 
-        if event.type == pygame.USEREVENT:
-            if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
-                if event.ui_element == hello_button:
-                    pygame.draw.rect(window_surface,color='blue',rect=parkrect)
-                    pygame.display.update()
+        
 
         manager.process_events(event)
+
+    pygame.draw.rect(window_surface,color='blue',rect=Parkrect)
+
 
     manager.update(time_delta)
 
